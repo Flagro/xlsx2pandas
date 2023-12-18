2083f43e9425ef8d77df13059c89bb8a44a965ad
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-12-18 22:26:44 +0100

    moved prettify output logic to utils

--- projecttextexcel/extractor.py
+++ projecttextexcel/extractor.py
@@ -2,12 +2,13 @@ import pandas as pd
 from typing import Optional, List, Dict, Union
 from openpyxl import load_workbook
 from .strategies.utils import get_strategy_parser
-from .utils import get_in_memory_file
+from .utils import get_in_memory_file, prettify_workbook_dataframes_output
 
 
 def get_df(file_path, 
            sheet_names: Optional[Union[str, List[str]]], 
-           strategy='general', 
+           strategy='general',
+           prettify_output=True,
            **kwargs) -> Union[List[pd.DataFrame], Dict[str, List[pd.DataFrame]], Dict[str, pd.DataFrame], pd.DataFrame]:
     """
     Returns a list or a single pandas dataframe of the extracted data from the xlsx/xlsm file.
@@ -33,12 +34,10 @@ def get_df(file_path,
         else:
             ws = wb[sheet]
             sheet_dataframes = strategy_parser.parse(ws, **kwargs)
-            if len(sheet_dataframes) == 1:
-                dataframes_dict[sheet] = sheet_dataframes[0]
-            else:
-                dataframes_dict[sheet] = sheet_dataframes
+            dataframes_dict[sheet] = sheet_dataframes
     wb.close()
-    if len(dataframes_dict) == 1:
-        return dataframes_dict[dataframes_dict.keys()[0]]
-    else:
-        return dataframes_dict
+
+    if prettify_output:
+        dataframes_dict = prettify_workbook_dataframes_output(dataframes_dict)
+
+    return dataframes_dict
--- projecttextexcel/utils.py
+++ projecttextexcel/utils.py
@@ -8,3 +8,16 @@ def get_in_memory_file(file_path):
     with open(file_path, "rb") as f:
         in_memory_file = io.BytesIO(f.read())
     return in_memory_file
+
+
+def prettify_workbook_dataframes_output(dataframes_dict):
+    output = dict()
+    for sheet, dataframes in dataframes_dict.items():
+        if len(dataframes) == 1:
+            output[sheet] = dataframes[0]
+        else:
+            output[sheet] = dataframes
+    if len(output) == 1:
+        return list(output.values())[0]
+    else:
+        return output
