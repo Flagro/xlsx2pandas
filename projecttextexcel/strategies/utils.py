9ee9dafcd0dbb391a41ec28fbe05984845c393de
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-12-18 22:21:38 +0100

    initialized base parser and importlib imports

---
+++ projecttextexcel/strategies/utils.py
@@ -0,0 +1,28 @@
+import importlib
+from abc import ABC, abstractmethod
+
+
+class BaseExcelParser(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def get_dataframes(self, **kwargs):
+        pass
+
+
+def get_strategy_parser(strategy):
+    try:
+        # Dynamically import the strategy class
+        strategy_module = importlib.import_module(f".{strategy}", __package__)
+    except ImportError:
+        raise ValueError(f"Strategy {strategy} not found.")
+
+    # Get the strategy class
+    strategy_class = getattr(strategy_module, "ExcelParser")
+    if not issubclass(strategy_class, BaseExcelParser):
+        raise TypeError("Invalid strategy type.")
+
+    # Create an instance of the strategy and parse the file
+    strategy = strategy_class()
+    return strategy
