e706048fa379245e73bebeaea79a50df0b567b32
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-12-18 23:18:37 +0100

    Implemented template for header separation logic

---
+++ projecttextexcel/header_separation/general_separator.py
@@ -0,0 +1,5 @@
+from .utils import BaseHeaderSeparator
+
+class GeneralHeaderSeparator(BaseHeaderSeparator):
+    def get_header_rows_cnt(self, openpyxl_ws, table_range, **kwargs):
+        pass
---
+++ projecttextexcel/header_separation/utils.py
@@ -0,0 +1,28 @@
+import importlib
+from abc import ABC, abstractmethod
+
+
+class BaseHeaderSeparator(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def get_header_rows_cnt(self, openpyxl_ws, table_range, **kwargs):
+        pass
+
+
+def get_header_separator(strategy):
+    try:
+        # Dynamically import the strategy class
+        strategy_module = importlib.import_module(f".{strategy}_separator", __package__)
+    except ImportError:
+        raise ValueError(f"Strategy {strategy} not found.")
+
+    # Get the strategy class
+    strategy_class = getattr(strategy_module, "ExcelParser")
+    if not issubclass(strategy_class, BaseHeaderSeparator):
+        raise TypeError("Invalid strategy type.")
+
+    # Create an instance of the strategy and parse the file
+    strategy = strategy_class()
+    return strategy
