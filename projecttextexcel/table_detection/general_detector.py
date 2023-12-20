8c6bd52151aa0834c7df82d825d007a5f82e0596
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-12-20 16:27:47 +0100

    implemented basic general detector

--- projecttextexcel/table_detection/general_detector.py
+++ projecttextexcel/table_detection/general_detector.py
@@ -1,5 +1,46 @@
+from openpyxl.utils import get_column_letter
 from .utils import BaseTableDetector
+
+
+def is_empty(cell):
+    return cell is None or cell.value is None
+
 
 class TableDetector(BaseTableDetector):
     def get_table_ranges(self, openpyxl_ws, **kwargs):
-        pass
+
+        tables = []
+        visited = set()
+
+        for row in openpyxl_ws.iter_rows():
+            for cell in row:
+                if cell.coordinate in visited or is_empty(cell):
+                    continue
+
+                # Initialize boundaries of the table
+                top, left = cell.row, cell.column
+                bottom, right = top, left
+
+                # Expand to the right
+                for j in range(left + 1, openpyxl_ws.max_column + 1):
+                    if is_empty(openpyxl_ws.cell(row=top, column=j)):
+                        break
+                    right = j
+
+                # Expand downwards
+                for i in range(top + 1, openpyxl_ws.max_row + 1):
+                    if all(is_empty(openpyxl_ws.cell(row=i, column=j)) for j in range(left, right + 1)):
+                        break
+                    bottom = i
+
+                # Add found table range
+                start = f"{get_column_letter(left)}{top}"
+                end = f"{get_column_letter(right)}{bottom}"
+                tables.append(f"{start}:{end}")
+
+                # Mark cells as visited
+                for i in range(top, bottom + 1):
+                    for j in range(left, right + 1):
+                        visited.add(openpyxl_ws.cell(row=i, column=j).coordinate)
+
+        return tables
