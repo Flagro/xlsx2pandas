072710e5d299df41189b5ecbbe14eb39c4470b8e
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-12-20 16:27:59 +0100

    implemented basic general separator

--- projecttextexcel/header_separation/general_separator.py
+++ projecttextexcel/header_separation/general_separator.py
@@ -2,4 +2,4 @@ from .utils import BaseHeaderSeparator
 
 class HeaderSeparator(BaseHeaderSeparator):
     def get_header_rows_cnt(self, openpyxl_ws, table_range, **kwargs):
-        pass
+        return 1
