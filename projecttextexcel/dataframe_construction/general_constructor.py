66184fbd32cb618509725c7081773ca52d1f7e0f
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-12-25 23:24:43 +0100

    multirow header handling

--- projecttextexcel/dataframe_construction/general_constructor.py
+++ projecttextexcel/dataframe_construction/general_constructor.py
@@ -1,6 +1,13 @@
 import pandas as pd
 from .utils import BaseDataFrameConstructor
 
+
+def join_header_rows(header_rows):
+    """
+    Joins a list of header rows into a single list of strings by underscore.
+    """
+    return [str("_".join([str(cell) for cell in row])) for row in header_rows]
+
 class DataFrameConstructor(BaseDataFrameConstructor):
     def construct_dataframe(self, openpyxl_ws, table_range, header_rows_cnt, **kwargs):
         data = []
@@ -8,5 +15,5 @@ class DataFrameConstructor(BaseDataFrameConstructor):
             data.append([cell.value for cell in row])
 
         # Construct the dataframe
-        df = pd.DataFrame(data[header_rows_cnt:], columns=data[0:header_rows_cnt][0])
+        df = pd.DataFrame(data[header_rows_cnt:], columns=join_header_rows(data[0:header_rows_cnt]))
         return df
