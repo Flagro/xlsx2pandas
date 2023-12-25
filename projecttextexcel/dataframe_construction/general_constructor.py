01de2163d79373f9e210bedfe9a3e9ec9841d307
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-12-26 00:24:04 +0100

    empty dataframe constructor fix

--- projecttextexcel/dataframe_construction/general_constructor.py
+++ projecttextexcel/dataframe_construction/general_constructor.py
@@ -2,12 +2,11 @@ import pandas as pd
 from .utils import BaseDataFrameConstructor
 
 
-def join_header_rows(header_rows):
+def join_header_rows(header_rows, cols_cnt):
     """
     Joins a list of header rows into a single list of strings by underscore.
     """
-    n = len(header_rows[0])
-    headers = [""] * n
+    headers = [""] * cols_cnt
     for row in header_rows:
         for i, cell in enumerate(row):
             if cell is not None:
@@ -20,7 +19,10 @@ class DataFrameConstructor(BaseDataFrameConstructor):
         data = []
         for row in openpyxl_ws[table_range]:
             data.append([cell.value for cell in row])
+        
+        if not data:
+            return pd.DataFrame()
 
         # Construct the dataframe
-        df = pd.DataFrame(data[header_rows_cnt:], columns=join_header_rows(data[0:header_rows_cnt]))
+        df = pd.DataFrame(data[header_rows_cnt:], columns=join_header_rows(data[0:header_rows_cnt], len(data[0])))
         return df
